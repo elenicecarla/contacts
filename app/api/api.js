becb17c6805435fcd34d25c782247a9c8598a1c8
Douglas Caruso <Douglas Caruso>
2016-12-06 19:43:52 -0200

    Edit page

--- app/api/api.js
+++ app/api/api.js
@@ -20,5 +20,20 @@ router.get("/delete/:cpf", function(req, res){
     
 })
 
+router.get("/edit/:cpf", function(req, res){
+     let cpf = req.params.cpf;
+     req.getConnection(function (err, connection) {
+      connection.query("SELECT * FROM contatos WHERE cpf = "+ cpf , function(err, rows)
+        {
+            
+             if(err)
+                 console.log("Error deleting : %s ",err );
+            console.log(rows);
+             res.render("editar",{contato:rows});
+             
+        });
+})
+});
+
 module.exports = router;
 
